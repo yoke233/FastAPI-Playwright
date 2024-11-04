e31dd65ca6b91b95596ccebae7887ac59fdfd485
yoke白板 <yoke2012@gmail.com>
2024-11-04 17:50:03 +0800

    feat: 重构代码并添加新功能
    - 重构 Dockerfile，使用新的 Python 3.11 镜像
    - 优化 main.py 文件结构，提高代码可读性
    - 添加新功能：获取页面 meta 信息
    - 改进浏览器实例管理，提高性能
    - 更新 requirements.txt，添加新的依赖项

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,20 @@
-FROM python:3.9.13-slim
+FROM docker.starringshop.top/library/python:3.11-slim-bookworm
 
-# 配置国内镜像源
-RUN sed -i 's|http://deb.debian.org|http://mirrors.aliyun.com|g' /etc/apt/sources.list && \
-    sed -i 's|http://security.debian.org|http://mirrors.aliyun.com|g' /etc/apt/sources.list
+# 创建并编辑 sources.list 文件
+RUN sed -i 's@deb.debian.org@mirrors.aliyun.com@g' /etc/apt/sources.list.d/debian.sources
+
+# 更新软件包列表
+RUN apt-get update
 
 WORKDIR /app
-COPY . /app
+COPY ./requirements.txt /app
 
 RUN pip install --no-cache-dir -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/
 
 # 安装 Playwright 及其依赖
 RUN python -m playwright install --with-deps
+
+COPY . /app
 
 # 设置环境变量
 ENV HOST=0.0.0.0
--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-import os
 import asyncio
 import base64
+import os
 import platform
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel, Field
+from contextlib import asynccontextmanager
 from typing import List, Optional
-from playwright.async_api import async_playwright, TimeoutError as PlaywrightTimeoutError
-from uvicorn import Config, Server
 
-app = FastAPI()
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
+from playwright.async_api import TimeoutError as PlaywrightTimeoutError
+from playwright.async_api import async_playwright
+from pydantic import BaseModel, Field
+from uvicorn import Config, Server
 
 
 class SearchIn(BaseModel):
     search: bool = False
-    search_input_selector: Optional[str] = Field(default=None, description="搜索输入框的选择器")
-    search_button_selector: Optional[str] = Field(default=None, description="搜索按钮的选择器")
+    search_input_selector: Optional[str] = Field(
+        default=None, description="搜索输入框的选择器"
+    )
+    search_button_selector: Optional[str] = Field(
+        default=None, description="搜索按钮的选择器"
+    )
     search_term: Optional[str] = Field(default=None, description="搜索关键词")
 
 
@@ -28,14 +33,23 @@ class ItemsConfig(BaseModel):
 
 class BodyConfig(BaseModel):
     enabled: bool = False
-    body_selectors: List[str] = Field(default_factory=list, description="正文内容的选择器列表")
-    title_selectors: List[str] = Field(default_factory=list, description="标题内容的选择器列表")
-    date_selectors: List[str] = Field(default_factory=list, description="日期内容的选择器列表")
+    body_selectors: List[str] = Field(
+        default_factory=list, description="正文内容的选择器列表"
+    )
+    title_selectors: List[str] = Field(
+        default_factory=list, description="标题内容的选择器列表"
+    )
+    date_selectors: List[str] = Field(
+        default_factory=list, description="日期内容的选择器列表"
+    )
 
 
 class RequestBody(BaseModel):
     url: str
-    browser: str = Field(default="chromium", description="要使用的浏览器类型: 'chromium', 'firefox', 或 'webkit'")
+    browser: str = Field(
+        default="chromium",
+        description="要使用的浏览器类型: 'chromium', 'firefox', 或 'webkit'",
+    )
     screenshot: bool = False
     search_in: SearchIn = SearchIn()
     items_config: ItemsConfig = ItemsConfig()
@@ -44,7 +58,7 @@ class RequestBody(BaseModel):
 
 async def wait_for_network_idle(page, timeout):
     try:
-        await asyncio.wait_for(page.wait_for_load_state('networkidle'), timeout=timeout)
+        await asyncio.wait_for(page.wait_for_load_state("networkidle"), timeout=timeout)
     except asyncio.TimeoutError:
         print(f"在 {timeout} 秒内未达到网络空闲状态，继续执行...")
 
@@ -52,9 +66,9 @@ async def wait_for_network_idle(page, timeout):
 def format_size(size):
     if size < 1024:
         return f"{size} B"
-    elif size < 1024 ** 2:
+    elif size < 1024**2:
         return f"{size / 1024:.2f} KB"
-    elif size < 1024 ** 3:
+    elif size < 1024**3:
         return f"{size / 1024 ** 2:.2f} MB"
     return f"{size / 1024 ** 3:.2f} GB"
 
@@ -63,14 +77,13 @@ async def capture_screenshot(page):
     screenshot = await page.screenshot()
     screenshot_size = len(screenshot)
     formatted_size = format_size(screenshot_size)
-    screenshot_base64 = base64.b64encode(screenshot).decode('utf-8')
-    return {
-        "size": formatted_size,
-        "base64": screenshot_base64
-    }
+    screenshot_base64 = base64.b64encode(screenshot).decode("utf-8")
+    return {"size": formatted_size, "base64": screenshot_base64}
 
 
-async def get_items_content(page, item_selector, title_selector=None, date_selector=None):
+async def get_items_content(
+    page, item_selector, title_selector=None, date_selector=None
+):
     await page.wait_for_selector(item_selector, timeout=10000)
     items = await page.query_selector_all(item_selector)
     items_content = []
@@ -80,22 +93,28 @@ async def get_items_content(page, item_selector, title_selector=None, date_selec
         if title_selector:
             title_element = await item.query_selector(title_selector)
             if title_element:
-                item_content['title'] = (await title_element.text_content()).replace("\n", " ").strip()
+                item_content["title"] = (
+                    (await title_element.text_content()).replace("\n", " ").strip()
+                )
             else:
-                item_content['title'] = "无标题"
+                item_content["title"] = ""
 
         link_elements = await item.query_selector_all("a")
         if link_elements:
-            item_content['links'] = [await link.get_attribute("href") for link in link_elements]
+            item_content["links"] = [
+                await link.get_attribute("href") for link in link_elements
+            ]
         else:
-            item_content['links'] = ["无链接"]
+            item_content["links"] = [""]
 
         if date_selector:
             date_element = await item.query_selector(date_selector)
             if date_element:
-                item_content['date'] = (await date_element.text_content()).replace("\n", " ").strip()
+                item_content["date"] = (
+                    (await date_element.text_content()).replace("\n", " ").strip()
+                )
             else:
-                item_content['date'] = "无日期"
+                item_content["date"] = "无日期"
 
         items_content.append(item_content)
     return items_content
@@ -116,74 +135,134 @@ async def get_content_by_selectors(page, selectors):
 
 
 async def get_body_content(page, body_selectors, title_selectors, date_selectors):
-    body_content = await get_content_by_selectors(page, body_selectors) if body_selectors else [
-        {"selector": None, "content": "无正文内容"}]
-    title_content = await get_content_by_selectors(page, title_selectors) if title_selectors else [
-        {"selector": None, "content": "无标题"}]
-    date_content = await get_content_by_selectors(page, date_selectors) if date_selectors else [
-        {"selector": None, "content": "无日期"}]
+    if body_selectors:
+        body_content = await get_content_by_selectors(page, body_selectors)
+    else:
+        all_text = await page.content()
+        body_content = [{"selector": None, "content": all_text}]
+    title_content = (
+        await get_content_by_selectors(page, title_selectors)
+        if title_selectors
+        else [{"selector": None, "content": ""}]
+    )
+    date_content = (
+        await get_content_by_selectors(page, date_selectors)
+        if date_selectors
+        else [{"selector": None, "content": ""}]
+    )
 
-    return {
-        "title": title_content,
-        "body": body_content,
-        "date": date_content
-    }
+    return {"title": title_content, "body": body_content, "date": date_content}
 
 
-async def get_page_info(request_data: RequestBody):
+async def get_meta(page):
+
+    # 获取所有的 <meta> 标签
+    meta_tags = await page.query_selector_all("meta")
+    # print(meta_tags)
+    meta_info = {}
+
+    for tag in meta_tags:
+        name = await tag.get_attribute("name")
+        property_attr = await tag.get_attribute("property")
+        content = await tag.get_attribute("content")
+
+        if name == "description" or property_attr == "og:description":
+            meta_info["description"] = content
+        elif name == "keywords" or property_attr == "og:keywords":
+            meta_info["keywords"] = content
+        elif property_attr == "og:title":
+            meta_info["title"] = content
+    return meta_info
+
+
+async def get_page_info(request_data: RequestBody, request: Request):
+
     page_info = {}
-    async with async_playwright() as p:
-        browser_type = getattr(p, request_data.browser, None)
-        if not browser_type:
-            raise HTTPException(status_code=400, detail="无效的浏览器类型")
-        browser = await browser_type.launch(headless=True)
-        page = await browser.new_page()
+    browser_type = request_data.browser
 
-        await page.goto(request_data.url)
+    if browser_type not in ["chromium", "firefox", "webkit"]:
+        raise HTTPException(status_code=400, detail="无效的浏览器类型")
 
-        if request_data.search_in.search:
-            try:
-                await page.fill(request_data.search_in.search_input_selector, request_data.search_in.search_term)
-                await page.click(request_data.search_in.search_button_selector)
-            except Exception as e:
-                print(f"搜索操作失败: {e}")
+    # 如果还没有启动该类型的浏览器，则启动它
+    if browser_type not in app.state.browser_instances:
+        p = request.app.state.playwright
+        use_browser = getattr(p, request_data.browser, None)
+        browser = await use_browser.launch(headless=True)
+        app.state.browser_instances[browser_type] = browser
+    else:
+        browser = app.state.browser_instances[browser_type]
 
-        await wait_for_network_idle(page, timeout=10)
+    page = await browser.new_page()
 
-        if request_data.items_config.enabled and request_data.body_config.enabled:
-            raise HTTPException(status_code=400, detail="列表页和详情页配置只能启用一个")
+    await page.goto(request_data.url)
 
-        if request_data.items_config.enabled:
-            try:
-                page_info["items"] = await get_items_content(
-                    page,
-                    request_data.items_config.item_selector,
-                    request_data.items_config.title_selector,
-                    request_data.items_config.date_selector)
-            except PlaywrightTimeoutError:
-                page_info["items"] = []
+    page_info["meta"] = await get_meta(page)
 
-        if request_data.body_config.enabled:
-            page_info["body"] = await get_body_content(
+    if request_data.search_in.search:
+        try:
+            await page.fill(
+                request_data.search_in.search_input_selector,
+                request_data.search_in.search_term,
+            )
+            await page.click(request_data.search_in.search_button_selector)
+        except Exception as e:
+            print(f"搜索操作失败: {e}")
+
+    await wait_for_network_idle(page, timeout=10)
+
+    if request_data.items_config.enabled and request_data.body_config.enabled:
+        raise HTTPException(status_code=400, detail="列表页和详情页配置只能启用一个")
+
+    if request_data.items_config.enabled:
+        try:
+            page_info["items"] = await get_items_content(
                 page,
-                request_data.body_config.body_selectors,
-                request_data.body_config.title_selectors,
-                request_data.body_config.date_selectors)
+                request_data.items_config.item_selector,
+                request_data.items_config.title_selector,
+                request_data.items_config.date_selector,
+            )
+        except PlaywrightTimeoutError:
+            page_info["items"] = []
 
-        if request_data.screenshot:
-            page_info["screenshot"] = await capture_screenshot(page)
+    if request_data.body_config.enabled:
+        page_info["body"] = await get_body_content(
+            page,
+            request_data.body_config.body_selectors,
+            request_data.body_config.title_selectors,
+            request_data.body_config.date_selectors,
+        )
 
-        await browser.close()
+    if request_data.screenshot:
+        page_info["screenshot"] = await capture_screenshot(page)
+
+    await page.close()
     return page_info
 
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with async_playwright() as p:
+        app.state.playwright = p
+        app.state.browser_instances = {}
+        print("Initialized browser_instances")  # 确认初始化
+
+        yield  # 先保留 playwright 实例
+
+        # 在应用关闭时关闭所有浏览器实例
+        for browser in app.state.browser_instances.values():
+            await browser.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
 @app.post("/capture")
-async def capture(request_data: RequestBody):
+async def capture(request_data: RequestBody, request: Request):
     if not request_data.url:
         raise HTTPException(status_code=400, detail="URL是必须的")
     if request_data.items_config.enabled and request_data.body_config.enabled:
         raise HTTPException(status_code=400, detail="列表页和详情页配置只能启用一个")
-    page_info = await get_page_info(request_data)
+    page_info = await get_page_info(request_data, request=request)
     return JSONResponse(content=page_info)
 
 
@@ -194,6 +273,7 @@ def run_server():
 
     if platform.system() == "Windows":
         from asyncio.windows_events import ProactorEventLoop
+
         loop = ProactorEventLoop()
         asyncio.set_event_loop(loop)
     else:
